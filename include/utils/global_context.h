a329411fd77d2db05ac9058353129d57be81780e
wangwei <wangwei.cs@gmail.com>
2014-06-28 15:05:03 +0800

    add coding rules libraries to readme; create GlobalContext class; finsh pseudo code for main.cc and start_deamon.cc;

--- include/utils/global_context.h
+++ include/utils/global_context.h
@@ -1,17 +1,39 @@
-#ifndef LAPIS_UTILS_GLOBAL_CONTEXT
-#define LAPIS_UTILS_GLOBAL_CONTEXT
+// Copyright © 2014 Wei Wang. All Rights Reserved.
+// 2014-06-28 14:58
+#ifndef INCLUDE_UTILS_GLOBAL_CONTEXT_H_
+#define INCLUDE_UTILS_GLOBAL_CONTEXT_H_
+
+#include <map>
+#include <utility>
+
+namespace lapis {
+enum Role {
+  kCoordinator,
+  kWoker,
+  kMemoryServer,
+  kDiskServer
+};
 
 class GlobalContext {
  public:
+  explicit GlobalContext(const char* system_conf_path,
+                         const char * model_conf_path);
+  inline bool IsRoleOf(const Role& role, int rank);
+
+  int num_memory_servers() { return num_memory_servers_; }
+  int num_disk_servers() { return num_disk_servers_; }
+  const char* model_conf_path() {return model_conf_path_; }
 
  private:
-  int num_workers; 
-  
-  //# of nodes working on serving the distributed memory
+  // map from role to (start_rank, end_rank) pair
+  std::map<Role, std::pair<int, int> > role_rank_;
+  // # of nodes have memory tables
   int num_memory_servers;
-
-  //# of nodes working on serving the distributed disk
+  // # of nodes have disk tables
   int num_disk_servers;
-};
 
-#endif //LAPIS_UTILS_GLOBAL_CONTEXT 
+  const char* model_conf_path_;
+};
+}  // namespace lapis
+
+#endif  // INCLUDE_UTILS_GLOBAL_CONTEXT_H_
---
+++ include/utils/proto_helper.h
@@ -0,0 +1,18 @@
+// Copyright © 2014 Wei Wang. All Rights Reserved.
+// 2014-06-28 14:57
+#ifndef INCLUDE_UTILS_PROTO_HELPER_H_
+#define INCLUDE_UTILS_PROTO_HELPER_H_
+
+#include <google/protobuf/message.h>
+
+using google::protobuf::Message;
+
+namespace lapis {
+void ReadProtoFromTextFile(const char* filename, Message* proto);
+void WriteProtoToTextFile(const Message& proto, const char* filename);
+void ReadProtoFromBinaryFile(const char* filename, Message* proto);
+void WriteProtoToBinaryFile(const Message& proto, const char* filename);
+}  // namespace lapis
+
+#endif  // INCLUDE_UTILS_PROTO_HELPER_H_
+

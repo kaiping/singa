f7935d29487be49b6f33f792e8622be6a065e47e
WANG Sheng <wangsheng1001@gmail.com>
2015-01-14 21:50:11 +0800

    darray single-node version. need to further test it.

---
+++ include/darray/larray.h
@@ -0,0 +1,96 @@
+#ifndef INCLUDE_DA_LARRAY_H_
+#define INCLUDE_DA_LARRAY_H_
+
+#include <memory>
+#include <chrono>
+#include <random>
+#include <cmath>
+#include "darray/arraycomm.h"
+
+namespace singa{
+class LArray{
+
+  friend class DArray;
+
+  public:
+  /****************
+   * constructors *
+   ****************/
+  LArray();
+  LArray(const Shape& shp, float* addr);
+  LArray(const LArray& other);
+  LArray(LArray&& other);
+  /*************
+   * operators *
+   *************/
+  LArray& operator=(const LArray& other);
+  LArray& operator=(LArray&& other);
+  LArray operator[](int k) const;
+  /***********
+   * methods *
+   ***********/
+  //setup
+  void Assign(const Shape& shp, float* addr);
+  void Assign(Shape&& shp, float* addr);
+  //fill
+  void Fill(float val);
+  void RandUniform(float low, float high);
+  void RandGaussian(float mean, float std);
+  void CopyFrom(const LArray& src);
+  //void CopyFrom(const LArray& src, const Range rng);
+  //operation
+  size_t dim() const;
+  size_t vol() const;
+  float* dptr() const;
+  void ToSub(int k);
+  //math
+  void Add(const LArray& src1, const LArray& src2);
+  void Add(const LArray& src, const float v);
+  void Add(const LArray& src);
+  void Add(const float v);
+  void Minus(const LArray& src1, const LArray& src2);
+  void Minus(const LArray& src, const float v);
+  void Minus(const LArray& src);
+  void Minus(const float v);
+  void Mult(const LArray& src1, const LArray& src2);
+  void Mult(const LArray& src, const float v);
+  void Mult(const LArray& src);
+  void Mult(const float v);
+  void Div(const LArray& src1, const LArray& src2);
+  void Div(const LArray& src, const float v);
+  void Div(const LArray& src);
+  void Div(const float v);
+  void Square(const LArray& src);
+  void Pow(const LArray& src, const float p);
+  void Dot(const LArray& src1, const LArray& src2, bool trans1=false, bool trans2=false, bool overwrite=true);
+  void AddCol(const LArray& src);
+  void AddRow(const LArray& src);
+  void SumCol(const LArray& src, bool overwrite=true);
+  void SumRow(const LArray& src, bool overwrite=true);
+  void Sum(const LArray& src, const Pair& rng);
+  void Max(const LArray& src, const float v);
+  void Min(const LArray& src, const float v);
+  void Threshold(const LArray& src, const float v); 
+  void Map(std::function<float(float)> func, const LArray& src); 
+  void Map(std::function<float(float, float)> func, const LArray& src1, const LArray& src2);
+  void Map(std::function<float(float, float, float)> func, const LArray& src1, const LArray& src2, const LArray& src3);
+  float Sum() const;
+  float Max() const;
+  float Min() const;
+  float Norm1() const;
+  float* addr(int idx0) const;
+  float* addr(int idx0, int idx1) const;
+  float* addr(int idx0, int idx1, int idx2) const;
+  float* addr(int idx0, int idx1, int idx2, int idx3) const;
+  float& at(int idx0) const;
+  float& at(int idx0, int idx1) const;
+  float& at(int idx0, int idx1, int idx2) const;
+  float& at(int idx0, int idx1, int idx2, int idx3) const;
+  
+  private:
+  Shape shape_;
+  float* head_ = nullptr;
+};
+
+} // namespace singa
+#endif // INCLUDE_DA_LARRAY_H_
---
+++ src/darray/arraycomm.cc
@@ -0,0 +1,197 @@
+#include "darray/arraycomm.h"
+#include <glog/logging.h>
+
+using namespace std;
+
+namespace singa{
+
+/*********
+ * Shape *
+ *********/
+
+Shape::Shape() {}
+Shape::Shape(const Point& pt) : scale_(pt) { Init(); }
+Shape::Shape(Point&& pt) : scale_(move(pt)) { Init(); }
+Shape::Shape(const Shape& other){
+  scale_ = other.scale_;
+  base_ = other.base_;
+  vol_ = other.vol_;
+}
+Shape::Shape(Shape&& other){
+  scale_ = move(other.scale_);
+  base_ = move(other.base_);
+  vol_ = other.vol_;
+}
+
+Shape& Shape::operator=(const Shape& other){
+  if (this == &other) return *this;
+  scale_ = other.scale_;
+  base_ = other.base_;
+  vol_ = other.vol_;
+  return *this;
+}
+
+Shape& Shape::operator=(Shape&& other){
+  if (this == &other) return *this;
+  scale_ = move(other.scale_);
+  base_ = move(other.base_);
+  vol_ = other.vol_;
+  return *this;
+}
+
+int Shape::operator[](size_t i) const{
+  return scale_[i];
+}
+
+bool Shape::operator==(const Shape& other) const{
+  return scale_ == other.scale_;
+}
+
+bool Shape::operator!=(const Shape& other) const{
+  return scale_ != other.scale_;
+}
+
+size_t Shape::dim() const{
+  return scale_.size();
+}
+
+size_t Shape::vol() const{
+  return vol_;
+}
+
+Point Shape::point() const{
+  return scale_;
+}
+
+void Shape::Reassign(size_t dim, size_t v){
+  scale_[dim] = v;
+  Init();
+}
+
+Shape Shape::SubShape() const{
+  Point pt;
+  for (int i = 1; i < scale_.size(); ++i) pt.push_back(scale_[i]);
+  return Shape(pt);
+}
+
+size_t Shape::SubShapeVol() const{
+  return vol_/scale_[0];
+}
+
+void Shape::Init(){
+  if (!scale_.size()) return;
+  vol_ = 1;
+  base_.resize(scale_.size());
+  for (int i = scale_.size()-1; i >= 0; --i){
+    base_[i] = vol_;
+    vol_ *= scale_[i];
+  }
+}
+
+/*********
+ * Range *
+ *********/
+
+Range::Range(){}
+Range::Range(const Point& pt) : start_(Point(pt.size(),0)),end_(pt) {}
+Range::Range(Point&& pt) : start_(Point(pt.size(),0)),end_(move(pt)) {}
+Range::Range(const Point& st, const Point& ed) : start_(st),end_(ed) {}
+Range::Range(const Point& st, Point&& ed) : start_(st),end_(move(ed)) {}
+Range::Range(Point&& st, const Point& ed) : start_(move(st)),end_(ed) {}
+Range::Range(Point&& st, Point&& ed) : start_(move(st)),end_(move(ed)) {}
+Range::Range(const Range& other) : start_(other.start_),end_(other.end_) {}
+Range::Range(Range&& other) : start_(move(other.start_)),end_(move(other.end_)) {}
+
+Range& Range::operator=(const Range& other){
+  if (this == &other) return *this;
+  start_ = other.start_;
+  end_ = other.end_;
+  return *this;
+}
+
+Range& Range::operator=(Range&& other){
+  if (this == &other) return *this;
+  start_ = move(other.start_);
+  end_ = move(other.end_);
+  return *this;
+}
+
+Pair Range::operator[](size_t i) const{
+  return Pair(start_[i], end_[i]);
+}
+
+bool Range::operator==(const Range& other) const{
+  return start_ == other.start_ && end_ == other.end_;
+}
+
+bool Range::operator!=(const Range& other) const{
+  return !(*this == other);
+}
+
+size_t Range::dim() const{
+  return start_.size();
+}
+
+bool Range::IsValid() const{
+  if (start_.size() != end_.size()) return false;
+  for (int i = 0; i < start_.size(); ++i)
+    if (start_[i] > end_[i])
+      return false;
+  return true;
+}
+
+Range Range::Intersect(const Range& other) const{
+  Point st, ed;
+  int a, b;
+  for (int i = 0; i < start_.size(); ++i){
+    a = start_[i] > other.start_[i] ? start_[i] : other.start_[i];
+    b = end_[i] < other.end_[i] ? end_[i] : other.end_[i];
+    if (a >= b) a = b = 0;
+    st.push_back(a);
+    ed.push_back(b);
+  }
+  return Range(st, ed);
+}
+
+bool Range::IsInRange(const Point& pt) const{
+  for (int i = 0; i < pt.size(); ++i)
+    if (pt[i] < start_[i] || end_[i] <= pt[i])
+      return false;
+  return true;
+}
+
+/*************
+ * Partition *
+ *************/
+//TODO
+Partition::Partition(){}
+Partition::Partition(const Shape& shp, const Range& rng){}
+Partition::Partition(Shape&& shp, const Range& rng){}
+Partition::Partition(const Shape& shp, Range&& rng){}
+Partition::Partition(Shape&& shp, Range&& rng){}
+Partition::Partition(const Partition& other){}
+Partition::Partition(Partition&& other){}
+size_t Partition::dim() const{
+  return range_.dim();
+}
+size_t Partition::LocalVol() const{
+  return 0;
+}
+size_t Partition::TotalVol() const{
+  return 0;
+}
+bool Partition::IsValid() const{
+  return true;
+}
+bool Partition::IsInPartition(const Point& pt) const{
+  return true;
+}
+Shape Partition::GetShape() const{
+  return shape_;
+}
+Range Partition::GetRange() const{
+  return range_;
+}
+
+}
+
